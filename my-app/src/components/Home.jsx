8495741852a7bb8a5fac293b1df5cea7d039441b
Andrés Díaz <Adiazgomez68@correo.unicordoba.edu.co>
2021-10-01 18:19:23 -0500

    Le di un estilo al texto del Home

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -10,8 +10,14 @@ const Home = () => {
             <Header/>
 
             <div className="body">
-                <h1> MegaSales </h1>
-                <p> This is a web application for electronic product sales management </p>
+                <h1> Let's Go! </h1>
+                <p> 
+                    This is a web application for the sale of electronic products, 
+                    with crazy promotions and discounts. Enter now, and enjoy all the 
+                    products we have for you. To register you only need to have a Gmail account, 
+                    wait for the user permissions to be granted by the administrator, and that's it. 
+                    You're welcome!
+                </p>
             </div>
 
             <Footer/>
