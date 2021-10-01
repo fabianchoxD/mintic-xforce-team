3ded54d9037de217f293fb6e3b80550311b2e3c2
Andrés Díaz <Adiazgomez68@correo.unicordoba.edu.co>
2021-10-01 10:26:30 -0500

    Hice los cambios al css del footer y agregué la interfaz del login

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import '../styles/Footer.css';
 
 const Footer = () => {
     return (
-        <div className="container">
-            
+        <div className="footer">
+            <center><h6>footer</h6></center>
         </div>
     )
 }
--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,13 +1,115 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
+// import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-class Login extends React.Component {
+import '../styles/Login.css';
 
-    render() {
-        return (
-            <p> Aquí colocaré el login </p>
-        )
-    }
+const theme = createTheme();
 
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs" className="login">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 5,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end" sx={{marginBottom: 3}}>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default Login;
