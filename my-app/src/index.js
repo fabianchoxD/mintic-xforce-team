63a9465c0110d761e4ea63ae300288036b2eab4f
Andrés Díaz <Adiazgomez68@correo.unicordoba.edu.co>
2021-10-05 08:56:05 -0500

    Añadí ruta y componente de usuarios

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import Login from './pages/Login';
 import Home from './components/Home';
+import Users from './pages/Users';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ ReactDOM.render(
         <Route exact path="/" component={App}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path="/home" component={Home}/>
+        <Route exact path="/users" component={Users}/>
       </Switch>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ my-app/src/pages/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Users extends React.Component {
+
+    reder() {
+        return (
+            <h1> Hello World! </h1>
+        )
+    }
+
+}
+
+export default Users;
