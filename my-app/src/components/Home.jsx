111ad5e16ce2e998384d28e5d840887de20ad6a0
Fabian Varon <fabian.varon@cwc.com>
2021-10-06 00:31:28 -0500

    added carousel

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './Header';
 import Footer from "./Footer";
 import { Container, Row, Col } from "react-bootstrap"
+import Carousel from 'react-bootstrap/Carousel'
 
 import '../styles/Home.css'
 
@@ -13,8 +14,37 @@ const Home = () => {
                     <Header />
                 </Row>
                 <Row>
-                    <Col md="auto">
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="img/Banner1.png"
+                                alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="img/Banner2.png"
+                                alt="Second slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="img/Banner3.png"
+                                alt="Third slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                </Row>
+                <Row>
+                    <Col>
                         <h1 className="mainTittleH1"> Let's Go! </h1>
+                    </Col>
+                </Row>
+                <Row className="mb-5">
+                    <Col>
                         <p className="mainTittleP">
                             This is a web application for the sale of electronic products,
                             with crazy promotions and discounts. Enter now, and enjoy all the
@@ -24,8 +54,12 @@ const Home = () => {
                         </p>
                     </Col>
                 </Row>
+                <div>
+                </div>
+                <Row mb-5>
+                    <Footer />
+                </Row>
             </Container>
-            <Footer />
 
         </React.Fragment>
     )
