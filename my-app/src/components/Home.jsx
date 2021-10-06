b4db2efd9368bfe187ecd7e3e0d239f8eb85c783
Fabian Varon <fabian.varon@cwc.com>
2021-10-05 23:26:42 -0500

    added responsive Home

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import Header from './Header';
 import Footer from "./Footer";
+import { Container, Row, Col } from "react-bootstrap"
 
 import '../styles/Home.css'
 
 const Home = () => {
     return (
         <React.Fragment>
-            <Header/>
+            <Container className="fluid" fluid >
+                <Row>
+                    <Header />
+                </Row>
+                <Row>
+                    <Col md="auto">
+                        <h1 className="mainTittleH1"> Let's Go! </h1>
+                        <p className="mainTittleP">
+                            This is a web application for the sale of electronic products,
+                            with crazy promotions and discounts. Enter now, and enjoy all the
+                            products we have for you. To register you only need to have a Gmail account,
+                            wait for the user permissions to be granted by the administrator, and that's it.
+                            You're welcome!
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer />
 
-            <div className="body">
-                <h1> Let's Go! </h1>
-                <p> 
-                    This is a web application for the sale of electronic products, 
-                    with crazy promotions and discounts. Enter now, and enjoy all the 
-                    products we have for you. To register you only need to have a Gmail account, 
-                    wait for the user permissions to be granted by the administrator, and that's it. 
-                    You're welcome!
-                </p>
-            </div>
-
-            <Footer/>
         </React.Fragment>
     )
 }
