cd16a111ac0d7a4d5a471446fdc2463cbeaa68ca
Fabian Varon <fabian.varon@cwc.com>
2021-10-13 22:23:20 -0500

    added google login - button and logic

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -28,12 +28,53 @@ class Login extends Component {
     const responseErrorGoogle = (response) =>{
       console.log("MAL");
     }
+    return (
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Container component="main" maxWidth="xs" className="login">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Press button to Sign up with Google
+            </Typography>
+            <Box component="form">
+                <br/><br/>
+                <GoogleLogin
+                  clientId="871739763343-2miis7h4hc28ce17v7t4spaneq5tkg49.apps.googleusercontent.com"
+                  buttonText="Login"
+                  onSuccess={responseSuccessGoogle}
+                  onFailure={responseErrorGoogle}
+                  cookiePolicy={'single_host_origin'}
+                  render={renderProps => (
+                  <GoogleButton onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}>Sign in with Google</GoogleButton>
+                    )}
+                  />
+            </Box>
+            <Box sx={{
+              marginTop: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            </Box>
 
-        </Box>
-      </Container>
-      <Footer />
-    </ThemeProvider>
-  );
+          </Box>
+        </Container>
+        <Footer />
+      </ThemeProvider>
+    )
+  };
 }
 
 export default Login;
