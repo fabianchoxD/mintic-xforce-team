af4eabd4cb4ff93c9eca669e8cf327b8f28b5fd7
Fabian Varon <fabian.varon@cwc.com>
2021-10-14 22:48:00 -0500

    removed /products from path

--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -3,10 +3,10 @@ var productsRouter = express.Router();
 const ctrlProducts = require('../controllers/ctrlProducts.js');
 
 // PRODUCTS ROUTES
-productsRouter.post('/products', ctrlProducts.createProduct);
-productsRouter.get('/products', ctrlProducts.getProducts);
-productsRouter.get('/products/:id', ctrlProducts.getProductId);
-productsRouter.put('/products/:id', ctrlProducts.updateProduct);
-productsRouter.delete('/products/:id', ctrlProducts.deleteProduct);
+productsRouter.post('/', ctrlProducts.createProduct);
+productsRouter.get('/', ctrlProducts.getProducts);
+productsRouter.get('/:id', ctrlProducts.getProductId);
+productsRouter.put('/:id', ctrlProducts.updateProduct);
+productsRouter.delete('/:id', ctrlProducts.deleteProduct);
 
 module.exports = productsRouter;
