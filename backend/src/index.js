3e7ffc32861bfed89a813c2e357d49865f95e393
Fabian Varon <fabian.varon@cwc.com>
2021-10-14 22:17:22 -0500

    added libraries to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,14 @@
-const  app  = require('./routes');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+const mongoose = require('./database/Connection');
+const productsRouter = require('./routes/productsRouter');
+const usersRouter = require('./routes/usersRouter');
+app.use('/products', productsRouter);
+app.use('/users', usersRouter);
 
 app.listen(3001, () => {
     console.log("Server ON")
-}); 
+});
