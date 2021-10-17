f5dbd7a4b64f0ba2d9af76753373c21835dea752
Fabian Varon <fabian.varon@cwc.com>
2021-10-16 23:09:23 -0500

    code format

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -27,10 +27,17 @@ class Header extends React.Component {
         return (
             <AppBar className="appBar" position="fixed" elevation={4}>
                 <Toolbar>
-                    <Link to="/home" style={{ textDecoration: 'none', display: 'flex' }}>
-                        <Avatar alt="Bag" src="https://i.pinimg.com/736x/23/f7/c4/23f7c4409cb3b22e3e77f71ee7fefc9a.jpg" />
+                    <Link to="/home" style={{ textDecoration: "none", display: "flex" }}>
+                        <Avatar
+                            alt="Bag"
+                            src="https://i.pinimg.com/736x/23/f7/c4/23f7c4409cb3b22e3e77f71ee7fefc9a.jpg"
+                        />
                         <Typography
-                            style={{ fontFamily: 'Montserrat, sans-serif', color: 'white', marginTop: '3px' }}
+                            style={{
+                                fontFamily: "Montserrat, sans-serif",
+                                color: "white",
+                                marginTop: "3px",
+                            }}
                             variant="h6"
                             sx={{ flexGrow: 1 }}
                         >
