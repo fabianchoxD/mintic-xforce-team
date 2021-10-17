deb1c6e24fe2707ccf29260e9d074e75a079afe0
Fabian Varon <fabian.varon@cwc.com>
2021-10-16 23:12:46 -0500

    added token middleware file

---
+++ backend/src/middlewares/tokenMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+verifyToken = (req, res, next) => {
+    const { token } = req.headers;
+    console.log("middleware: ", token);
+    jwt.verify(token, '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', function(err, decoded){
+        if(err){
+            console.log('There was an error verifying token: ', err);
+            res.status(401).send('Not Authorized');
+            return;
+        }
+    console.log(decoded)
+    });
+    next();
+}
+
+module.exports ={
+    verifyToken
+}
