7dab04c2b8ca7f183b899e92bc3805394ff2ad67
Fabian Varon <fabian.varon@cwc.com>
2021-10-16 23:07:03 -0500

    added tokenMiddleware validation

--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var productsRouter = express.Router();
 const ctrlProducts = require('../controllers/ctrlProducts.js');
+const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 // PRODUCTS ROUTES
+productsRouter.use(tokenMiddleware.verifyToken);
 productsRouter.post('/', ctrlProducts.createProduct);
 productsRouter.get('/', ctrlProducts.getProducts);
 productsRouter.get('/:id', ctrlProducts.getProductId);
--- backend/src/routes/usersRouter.js
+++ backend/src/routes/usersRouter.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var usersRouter = express.Router();
 const ctrlUsers = require('../controllers/ctrlUsers');
+const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 // USERS ROUTES
+usersRouter.use(tokenMiddleware.verifyToken);
 usersRouter.post('/', ctrlUsers.createUser);
 usersRouter.get('/', ctrlUsers.getUsers);
 usersRouter.get('/:id',ctrlUsers.getUserId);
