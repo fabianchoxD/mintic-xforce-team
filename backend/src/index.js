6af84721898ad5dbd391c6409e22a31744cae50b
Fabian Varon <fabian.varon@cwc.com>
2021-10-16 23:03:43 -0500

    added authRouter route to index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,10 @@ app.use(express.json());
 const mongoose = require('./database/Connection');
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
+const authRouter = require('./routes/authenticationRouter');
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 app.listen(3001, () => {
     console.log("Server ON")
