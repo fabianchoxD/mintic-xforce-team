9fd299cfa3e53d20e91fbe778d848235be3f3cee
Fabian Varon <fabian.varon@cwc.com>
2021-10-24 02:34:58 -0500

    added afterLogin path

--- backend/src/routes/usersRouter.js
+++ backend/src/routes/usersRouter.js
@@ -7,6 +7,7 @@ const tokenMiddleware = require('../middlewares/tokenMiddleware');
 usersRouter.use(tokenMiddleware.verifyToken);
 usersRouter.post('/', ctrlUsers.createUser);
 usersRouter.get('/', ctrlUsers.getUsers);
+usersRouter.get('/afterLogin', ctrlUsers.getRoleAfterLogin);
 usersRouter.get('/:id',ctrlUsers.getUserId);
 usersRouter.put('/:id', ctrlUsers.updateUser);
 usersRouter.delete('/:id', ctrlUsers.deleteUser);
