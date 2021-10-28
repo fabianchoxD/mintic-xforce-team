3e34fcca59dcf9b83bc31d7f355c25325b51990c
Fabian S. Varon <fabianchoxd@gmail.com>
2021-10-27 22:30:00 -0500

    added middleware validation in salesRouter

--- xforce-mintic/src/routes/salesRouter.js
+++ xforce-mintic/src/routes/salesRouter.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var salesRouter = express.Router();
 const ctrlSales = require('../controllers/ctrlSales.js');
+const tokenMiddleware = require('../middlewares/tokenMiddleware');
 
 // SALES ROUTES
+salesRouter.use(tokenMiddleware.verifyToken);
 salesRouter.post('/', ctrlSales.createSale);
 salesRouter.get('/', ctrlSales.getSales);
 salesRouter.get('/:id', ctrlSales.getSaleId);
