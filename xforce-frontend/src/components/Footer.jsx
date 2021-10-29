e512dc22c4da7ea25c93f39c1885e344d0e4d223
Fabian S. Varon <fabianchoxd@gmail.com>
2021-10-28 23:16:37 -0500

    adding alt to images

--- xforce-frontend/src/components/Footer.jsx
+++ xforce-frontend/src/components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
             <Col />
             <Col>
                 <Link to={{ pathname: "https://www.misiontic2022.gov.co/portal/" }} target="_blank">
-                    <img src="img/logo_mision.png" id="img1" width="20%" />
+                    <img src="img/logo_mision.png" id="img1" width="20%" alt="misonTic"/>
                 </Link>
                 <Link to={{ pathname: "https://www.udea.edu.co/" }} target="_blank">
-                    <img src="img/udea_ingenia.png" id="img2" width="50%" />
+                    <img src="img/udea_ingenia.png" id="img2" width="50%" alt="udea"/>
                 </Link>
             </Col>
             <Col />
