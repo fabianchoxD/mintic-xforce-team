c2ed93eb9ffbdd6dec80c422abf0550483755802
Fabian S. Varon <fabianchoxd@gmail.com>
2021-10-28 23:21:42 -0500

    removed unused functions

--- xforce-frontend/src/miscellaneous/operationMsgResp.js
+++ xforce-frontend/src/miscellaneous/operationMsgResp.js
@@ -1,23 +1,5 @@
 import swal from "sweetalert";
 
-export function modifiedItemResponse(operation, description) {
-    swal(
-        "Successful Operation.",
-        operation + ": " + description + ", was successfully modified.",
-        "success"
-    );
-}
-
-export function createdItemResponse(description) {
-    swal(
-        "Successful Operation.",
-        description + ", added successfully.",
-        "success"
-    ).then(() => {
-        window.location.reload(true);
-    })
-}
-
 export function successRemoveResponse(operation) {
     swal(operation + " removed successfully.", {
         icon: "success",
